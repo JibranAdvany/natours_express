3f0b8239f6f1f7c8de03b68ac7a3ed6698813b31
Jibran Advani <jibranadvany@gmail.com>
2021-10-02 14:02:48 +0400

    implemented the sorting, filtering, pagination and field limiting in the API

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,8 +1,48 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+
+    // 1A. FILTERING (Basic)
+    // Created a hard copy of the object using destructuring.
+    const queryObj = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // Object.keys(object) - will return an array on which we can then iterate.
+    // below statement iterates over excluded fields and delete the respective object keys
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // 1B. FILTERING (Advanced)
+    let queryStr = JSON.stringify(queryObj); // to add $ sign
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2. SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy); // It will do in ascending order (-) for descending order
+    } else {
+      query = query.sort('-createdAt price');
+    }
+
+    // 3. FIELD LIMITING - only specific fields (Projecting)
+    if (req.query.fields) {
+      const fieldsSelect = req.query.fields.split(',').join(' ');
+      query = query.select(fieldsSelect);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4. PAGINATION
+    const maxResults = req.query.limit * 1 || 10;
+    const jumpResults = (req.query.page * 1 - 1) * maxResults || 0;
+    query = query.skip(jumpResults).limit(maxResults);
+
+    // Send response
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
---
+++ loadData.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+// Database connection
+const dbLink = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+mongoose.connect(dbLink, () => {
+  console.log('Database connected!');
+});
+
+// Reading the file (JSON)
+const tours = JSON.parse(
+  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
+);
+
+// Import data to Mongo
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+
+    console.log('Data successfully loaded.');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete already existing data
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+
+    console.log('Data successfully deleted.');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '--delete') {
+  deleteData();
+} else if (process.argv[2] == '--import') {
+  importData();
+}
