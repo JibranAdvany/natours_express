9060d59d8490bb1cc11cf3a6633b00569ba54657
Jibran Advani <jibranadvany@gmail.com>
2021-10-02 21:52:02 +0400

    implented basic global error handler

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 
 // custom middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 🔥');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 🔥');
+//   next();
+// });
 
 // Third-party middleware
 if (process.env.NODE_ENV == 'development') {
@@ -28,5 +28,31 @@ if (process.env.NODE_ENV == 'development') {
 // Routing middle ware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// catch-all routes
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can not find ${req.originalUrl} on this server.`,
+  // });
+
+  const err = new Error(`Can not find ${req.originalUrl} on this server.`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // If next function receives argument, express will direct the request to global error handler automatically.
+  next(err);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
