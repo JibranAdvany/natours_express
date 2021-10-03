f6200f8d98a1540459b1408cc4e091deec7a189c
Jibran Advani <jibranadvany@gmail.com>
2021-10-03 21:49:48 +0400

    implemented security good practicies like CSS and XSS attack prevention and nosql injection protections and rate limiting.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit'); // to protect against DOS attacks
+const helmet = require('helmet'); // secured HTTP headers
+const mongoSanitize = require('express-mongo-sanitize'); // protects against sql injections
+const xss = require('xss-clean'); // protects against malicious HTML attacks
+const hpp = require('hpp'); // protects against parameter pollution
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const errorController = require('./controllers/errorController');
@@ -10,11 +15,34 @@ const app = express();
 
 // Middleware
 
+// Helmet
+app.use(helmet());
+
 // Serving static assets
 app.use(express.static(`${__dirname}/public`));
 
 // Reading body data
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization middleware (against no sql query injection and against CSS attacks)
+app.use(mongoSanitize());
+
+// XSS
+app.use(xss());
+
+// Parameter Pollution Preventor
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // custom middleware
 // app.use((req, res, next) => {
@@ -29,6 +57,14 @@ app.use(express.json());
 // });
 
 // Third-party middleware
+// LIMITER
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour.',
+});
+app.use('/api', limiter);
+
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
