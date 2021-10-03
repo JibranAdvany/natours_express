770ea0c496173a438b985125c87d5514ddcf7d00
Jibran Advani <jibranadvany@gmail.com>
2021-10-03 12:20:31 +0400

    implemented signup feature with password hashing

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email ID.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter valid email address.'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minlength: [8, 'Password should have at least 8 characters.'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password.'],
+    validate: {
+      validator: function (val) {
+        return this.password === val;
+      }, // It will only work on save and create commands
+      message: 'Passwords provided do not match.',
+    },
+  },
+});
+
+// Encrypting the passwords. User passwords must not be saved as plain text (MOST IMPORTANT RULE)
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // if password is not modified.
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the password confirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
