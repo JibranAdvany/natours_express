0887ccf256bbca7ac32e895344149ce6cdfc3974
Jibran Advani <jibranadvany@gmail.com>
2021-10-04 22:24:06 +0400

    implemented nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourControllers = require('./../controllers/tourControllers');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
@@ -24,5 +25,15 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourControllers.deleteTour
   );
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 module.exports = router;
